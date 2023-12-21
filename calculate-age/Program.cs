1d6f1e1976f1e74b3fab353938819b17085346cb
anascode23 <anas.ali@ecutbildning.se>
2023-12-21 15:23:24 +0000

    generated console project and started on age calculator

---
+++ calculate-age/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Please enter when you were born to calculate you age: ");
+var input = Console.ReadLine();
+
+if (DateTime.TryParse(input, out var dateTime))
+{
+    int year = dateTime.Year;
+    Console.WriteLine("datetime object now" + year);
+}
+//Console.WriteLine(age);
